56fa26b70d2ebedb7d8a1de1772e21b7e217ca1f
min33sky <min33sky@naver.com>
2023-05-18 21:06:17 +0900

    feat: search result ui

--- src/components/LiveSearch.tsx
+++ src/components/LiveSearch.tsx
@@ -7,14 +7,18 @@ import { useQuery } from "@tanstack/react-query";
 export default function LiveSearch() {
   const [input, setInput] = useState("");
   const debounceText = useDebounce(input, 500);
+  const [isVisible, setIsVisible] = useState(false);
+
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const { isLoading } = useQuery({
+  const { data: results, isLoading } = useQuery({
     queryKey: ["person", debounceText],
     queryFn: () => getPerson(debounceText),
     enabled: !!debounceText,
+    keepPreviousData: true,
     onSuccess: (data) => {
       console.log(data);
+      setIsVisible(true);
     },
     onError: (error) => {
       console.log("Error: ", error);
@@ -27,8 +31,12 @@ export default function LiveSearch() {
 
   return (
     <div
-      className={`relative z-50 flex w-10/12 max-w-lg items-center rounded-full border-2 border-gray-500 px-4 py-3 text-lg transition duration-300 
-      ${input ? "border-transparent bg-slate-900" : "bg-transparent"}`}
+      className={`relative z-50 flex w-10/12 max-w-lg items-center border-2 px-4 py-3 text-lg transition duration-300
+      ${
+        input
+          ? "border-transparent bg-slate-900"
+          : "rounded-full border-gray-500 bg-transparent"
+      }`}
     >
       <MagnifyingGlassIcon className={`h-5 w-5 flex-shrink-0 text-gray-300`} />
       <input
@@ -44,7 +52,8 @@ export default function LiveSearch() {
 
       {input && isLoading && (
         <div
-          className={`h-4 w-4 animate-spin rounded-full border-2 border-gray-100`}
+          aria-label={"Loader"}
+          className={`mr-4 h-4 w-4 animate-spin rounded-full border-2 border-gray-100`}
         />
       )}
 
@@ -52,11 +61,28 @@ export default function LiveSearch() {
         <button
           onClick={() => {
             setInput("");
+            setIsVisible(false);
             inputRef.current?.focus();
           }}
         >
           <XMarkIcon className={`h-5 w-5 text-gray-300`} />
         </button>
+      )}
+      {isVisible && results && input && (
+        <ul className="absolute -left-[2px] -right-[2px] top-full border-0 border-slate-900 bg-slate-900">
+          {results?.map((item) => (
+            <li
+              key={item.name}
+              className="my-2 flex cursor-pointer items-center justify-between rounded-md p-4 transition duration-300 hover:bg-slate-700"
+              onClick={() => {
+                // onClose?.();
+              }}
+            >
+              <span>{item.name}</span>
+              <span className={`text-sm`}>{item.email}</span>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
